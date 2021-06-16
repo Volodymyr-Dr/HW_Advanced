115e1fb86b9e892f7daf58ce6402ee8757ba8b8f
Volodymyr Pavlyk <vv.dr.pvlk@gmail.com>
2021-06-16 19:45:59 +0300

    created 6 functions

--- arrays methods/script.js
+++ arrays methods/script.js
@@ -12,4 +12,86 @@ const getRandomArray = ((length, min, max) => {
 });
 console.log(getRandomArray());
 
-//
+// 2. Створіть функцію getModa(...numbers) – яка вираховує моду всіх переданих в неї аргументів.
+
+
+
+// 3. Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне всіх переданих в неї аргументів. НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
+
+let numbers = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2];
+
+const getAverage = () => {
+    numbers.filter((item) => {
+        return (Number.isInteger(item) || !item);
+    });
+    const total = numbers.reduce((total, number) => {
+        return total + number;
+    }, 0);
+    return total /  numbers.length;  
+};
+console.log(getAverage());
+
+// 4. Створіть функцію getMedian(...numbers) – яка рахує медіану всіх переданих в неї аргументів. НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
+ 
+let yourSetOfNumbers = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2];
+
+function getMedian(yourSetOfNumbers) {
+
+    yourSetOfNumbers.filter((item) => (Number.isInteger(item) || !item));
+
+    let count = 0;
+    
+    yourSetOfNumbers.sort((a, b) => a - b);
+
+    do {
+        yourSetOfNumbers.pop();
+        yourSetOfNumbers.shift();
+    } while (yourSetOfNumbers.length > 2)
+    
+    if (yourSetOfNumbers.length === 2) {
+        for (let i = 0; i < yourSetOfNumbers.length; i++) {
+            count = (yourSetOfNumbers[0] + yourSetOfNumbers[1]);
+            return count / 2;
+        }
+    }
+    return yourSetOfNumbers[0];
+}
+console.log(getMedian(yourSetOfNumbers))
+
+// 5. Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції
+
+let allNumbers = [1, 2, 3, 4, 5, 6];
+
+const getEvenNumbers = allNumbers.filter((item, index, arr) => {
+    return !(item % 2 === 0);
+});
+console.log(getEvenNumbers);
+
+// 6. Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість чисел більших 0.
+
+let setOfNumbers = [1, -2, 3, -4, -5, 6];
+
+const positiveNumber = setOfNumbers.filter((item) => {
+    return item > 0;
+});
+console.log(positiveNumber);
+
+// 7. Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті, які діляться на ціло на 5.
+
+let dividedByFive = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2];
+
+const getDividedByFive = dividedByFive.filter((item) => {
+    return item % 5 === 0;
+});
+console.log(getDividedByFive);
+// 8. Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*).
+
+
+
+// 9. Створіть функцію divideByThree(word), яка розбиває кожне слово на умовні склади по 3 букви. Якщо букв менше трьох – не розбиває. 
+
+
+
+// 10. Створіть функцію generateCombinations(word), яка видасть всі можливі перестановки(унікальні, без повторень) букв в слові.
+
+
